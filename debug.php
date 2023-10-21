21100435d72ce33c423573951ab7ce958e6ce9b2
Sina Eetezadi <code@eetezadi.com>
2023-10-21 04:36:45 +0000

    debug files

---
+++ debug.php
@@ -0,0 +1,17 @@
+<?php
+
+define('TEST_CONSTANT', 123);
+
+$anArray = array(1, 'test' => 2, 'test2' => ['t' => 123]);
+$aFloat = 1.23;
+$anInt = 123;
+$aString = '123';
+$anEmptyString = '';
+$aVeryLongString = str_repeat('lol', 1000);
+$aBoolean = true;
+$nullValue = null;
+$variableThatsNotSet;
+$aLargeArray = array_fill(0, 100, 'test');
+$arrayWithSpaceKey = array('space key' => 1);
+echo "HelloS";
+exit;
